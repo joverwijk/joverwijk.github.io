6e610fdc8c961815f740f011d7ff616f1b343f78
joverwijk <jarnooverwijk@gmail.com>
2018-08-30 13:33:49 +0200

    HTML -> PHP F1

---
+++ dashboard.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Ceci n'est pas un real dashboard yet</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+        <script src="js/main.js"></script>
+    </head>
+    <body>
+        <main>
+            <h1>Dashboard</h1>
+            <p>
+                Deze pagina bevat mijn persoonlijke dashboard. Het bevat onder
+                andere een voortgangsmeter en handige links.
+            </p>
+            <h2>Studiemonitor</h2>
+            <p>
+                <!-- Voortgangsbalk, evt. een grafiek o.i.d. -->
+            </p>
+            <h2>Studiewijzer</h2>
+            <p>
+                <!-- Zorg ervoor dat links eenvoudig kunnen worden gemaakt. -->
+            </p>
+        </main>
+    </body>
+</html>
