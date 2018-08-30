ad5416a7d849259ba263cd30ee2cc68290be07bb
joverwijk <jarnooverwijk@gmail.com>
2018-08-30 17:37:02 +0200

    + proberen de code iets overzichterlijker te maken

--- needs/nav.php
+++ needs/nav.php
@@ -5,10 +5,10 @@
     if (empty($active_dashboard)) $active_dashboard = '';
 
     // links
-    $nav_link_home = "<a" . $active_home . " href='index.<!-- FILEEXT -->'>Home</a>";
-    $nav_link_profile = "<a" . $active_profile . " href='profile.<!-- FILEEXT -->'>Profile</a>";
-    $nav_link_dashboard = "<a" . $active_dashboard . " href='dashboard.<!-- FILEEXT -->'>Dashboard</a>";
+    $nav_link_home = "<a" . $active_home . " href='index.<!-- FILEEXT -->'>Home</a>\n";
+    $nav_link_profile = "<a" . $active_profile . " href='profile.<!-- FILEEXT -->'>Profile</a>\n";
+    $nav_link_dashboard = "<a" . $active_dashboard . " href='dashboard.<!-- FILEEXT -->'>Dashboard</a>\n";
     
     // plaats nav
-    echo '<nav>' . $nav_link_home . $nav_link_profile . $nav_link_dashboard . '</nav>';
+    echo '<nav>' . $nav_link_home . $nav_link_profile . $nav_link_dashboard . "</nav>\n";
 ?>
