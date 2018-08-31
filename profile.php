0f308653c9b5088a51a3a94005c9173e7a6d7760
joverwijk <jarnooverwijk@gmail.com>
2018-09-01 01:59:27 +0200

    + kenmerken en korte intro

--- profile.php
+++ profile.php
@@ -14,10 +14,26 @@
         ?>
         <main>
             <section>
+                <h1>Over mij</h1>
                 <p>
-                    Ook hier komt binnenkort wat tekst. Nu is het nog een saaie,
-                    lege pagina. :(
+                    Ik ben Jarno Overwijk,
+                    <?php
+                        $dob = date_create('2000-12-01');
+                        $cur_date = date_create('now');
+                        $interval = date_diff($dob, $cur_date);
+                        echo $interval->format('%y jaar');
+                    ?>
+                    oud en volg de opleiding HBO-ICT aan de <a href="https://hz.nl">HZ</a> locatie Middelburg.
                 </p>
+                <h2>Kenmerken</h2>
+                <ol>
+                    <li>Algemeen</li>
+                    <ul>
+                        <li>Houd er totaal niet van om in de <i>spotlights</i> te staan.</li>
+                        <li>Presenteert daarom dus ook liever niet...</li>
+                        <li>Stelt met regelmaat privacy boven 'je mag alles weten'.</li>
+                    </ul>
+                </ol> <!-- TODO: uitbreiden met meer kenmerken en een afbeelding -->
             </section>
         </main>
     </body>
