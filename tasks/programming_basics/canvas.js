b9dc36e2cb3bba51d3b4dce1899f36e1f4b5c5c2
joverwijk <jarnooverwijk@gmail.com>
2018-09-10 14:23:31 +0200

    set-up voor canvas

---
+++ tasks/programming_basics/canvas.js
@@ -0,0 +1,8 @@
+/* Onderstaande code maakt een diamant */
+function drawDiamond() {
+    var canvas = document.getElementById("drawable_canvas"); // vind element
+    // do stuff here
+    // here
+    // and here
+    // and more here
+}
