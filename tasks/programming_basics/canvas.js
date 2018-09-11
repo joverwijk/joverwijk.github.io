9a1a3745414a3cf7c96bf91bcb94bdd4b028ce40
joverwijk <jarnooverwijk@gmail.com>
2018-09-11 11:13:07 +0200

    > JS maakt nu animatie

--- tasks/programming_basics/canvas.js
+++ tasks/programming_basics/canvas.js
@@ -1,8 +1,20 @@
 /* Onderstaande code maakt een diamant */
 function drawDiamond() {
     var canvas = document.getElementById("drawable_canvas"); // vind element
-    // do stuff here
-    // here
-    // and here
-    // and more here
+    var draw = canvas.getContext("2d");
+    // reset canvas, voor het geval dat
+    draw.clearRect(0, 0, canvas.width, canvas.height);
+    console.log("Canvas gereset");
+    draw.beginPath();
+    //           X   Y
+    draw.moveTo(250,500);
+    console.log("Cursor verplaatst naar (250,500)");
+    draw.strokeStyle="#00FF00";
+    draw.lineTo(500,250);
+    draw.lineTo(250,0);
+    draw.lineTo(0,250);
+    draw.lineTo(250,500);
+    console.log("Te tekenen lijnen en te gebruiken kleur geconfigureerd");
+    draw.stroke();
+    console.log("Diamant getekend!");
 }
